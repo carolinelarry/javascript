7434c631bdfcd5589c3987851b5217c370f8b8ba
Caroline Larry <carolinelarry@Carolines-MBP.attlocal.net>
2021-06-17 10:11:07 -0500

    Adding new files

---
+++ UFO-1/static/js/app.js
@@ -0,0 +1,62 @@
+// from data.js
+var tableData = data;
+
+//=========APPENDING DATA=========
+
+//Reference to body of table to append
+var tbody = d3.select("tbody");
+
+tableData.forEach((date) => {
+    var row = tbody.append("tr");
+    Object.entries(date).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+
+
+//===========FILTERING==========
+//Select button
+var filter_button = d3.select("#filter-btn");
+
+//Select the input form
+var input = d3.select("#filters");
+//try different id if this doesnt work
+
+//Event handlers
+filter_button.on("click", runEnter);
+input.on("submit", runEnter);
+
+//Creating function
+function runEnter() {
+    
+    //Prevent page from refreshing
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    //Get value of inputElement
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(date => date.datetime == inputValue);
+
+
+    console.log(filteredData);
+
+    tbody.html("");
+
+    filteredData.forEach((date) => {
+        var row = tbody.append("tr");
+        Object.entries(date).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+    //how to add data to the bottom
+    //d3.select("h1>span").text(inputValue);
+};
