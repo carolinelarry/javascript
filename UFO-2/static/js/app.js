5e5129ac78ffc990831cbd7714033a0ddb3633ee
Caroline Larry <carolinelarry@Carolines-MBP.attlocal.net>
2021-06-21 09:11:17 -0500

    Adding changes

--- UFO-2/static/js/app.js
+++ UFO-2/static/js/app.js
@@ -17,11 +17,12 @@ tableData.forEach((date) => {
 
 
 
-//===========FILTERING==========
+//===========FILTERING BY DATE==========
 //Select button
 var filter_button = d3.select("#filter-btn");
 
 //Select the input form
+//var input = d3.select(".filters");
 var input = d3.select(".filters");
 
 //Event handlers
@@ -56,6 +57,45 @@ function runEnter() {
         });
     });
 
-    //how to add data to the bottom
-    //d3.select("h1>span").text(inputValue);
+};
+
+//===========FILTERING BY CITY==========
+//Select button
+var filter_city_button = d3.select("#city-filter-btn");
+
+//Select the input form
+var city_input = d3.select(".city-filters");
+
+//Event handlers
+filter_city_button.on("click", runCityFilter);
+city_input.on("submit", runCityFilter);
+
+//Creating function
+function runCityFilter() {
+    
+    //Prevent page from refreshing
+    d3.event.preventDefault();
+
+    var inputElementCity = d3.select("#city");
+
+    //Get value of inputElement
+    var inputValueCity = inputElementCity.property("value");
+
+    console.log(inputValueCity);
+
+    var filteredCityData = tableData.filter(city => city.city == inputValueCity);
+
+
+    console.log(filteredCityData);
+
+    tbody.html("");
+
+    filteredCityData.forEach((city) => {
+        var cityRow = tbody.append("tr");
+        Object.entries(city).forEach(([key, value]) => {
+            var cityCell = cityRow.append("td");
+            cityCell.text(value);
+        });
+    });
+
 };
